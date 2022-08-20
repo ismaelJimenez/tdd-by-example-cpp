c368be24d6cdc13fc44998c571fa118095f041c5
Ismael <>
2022-08-20 18:19:46 +0000

    Add chapter 5

--- chapter-5/multi_currency_test.cpp
+++ chapter-5/multi_currency_test.cpp
@@ -3,27 +3,25 @@
 // All rights reserved.
 
 #include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
-#include "./dollar.hpp"
-#include "./franc.hpp"
+#include "chapter-5/dollar.hpp"
+#include "chapter-5/franc.hpp"
 
 using ::testing::Eq;
-using ::testing::Ne;
 
-TEST(MultipleCurrency, CanMultiplyADollarByANumber) {
+TEST(MultiCurrency, Multiplication) {
   Dollar five{5};
 
   ASSERT_THAT(five.times(2), Eq(Dollar{10}));
   ASSERT_THAT(five.times(3), Eq(Dollar{15}));
 }
 
-TEST(MultipleCurrency, CanCompareDollars) {
+TEST(MultiCurrency, Equality) {
   ASSERT_TRUE(Dollar{5} == Dollar{5});
   ASSERT_FALSE(Dollar{5} == Dollar{6});
 }
 
-TEST(MultipleCurrency, CanMultiplyAFrancByANumber) {
+TEST(MultiCurrency, FrancMultiplication) {
   Franc five{5};
 
   ASSERT_THAT(five.times(2), Eq(Franc{10}));
